05ded6c90ef6f75b7f530987a7b5a915c029bfff
Yatharth Chandel <yatharth.chandel.15012007@gmail.com>
2026-07-24 16:44:10 +0530

    Add kubernetes Deployment and Services to main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,12 @@ terraform {
 			source = "kreuzwerker/docker"
 			version = "~> 3.0"
 			}
-		}
+		kubernetes = {
+                        source = "hashicorp/kubernetes"
+                        version = "~> 2.23"
+                }
 	}
+}
 
 provider "docker" {}
 
@@ -22,3 +26,70 @@ resource "docker_container" "app_container" {
 		external = 3000
 	}
 }
+
+provider "kubernetes" {
+	config_path    = "~/.kube/config"
+}
+resource "kubernetes_deployment" "notes-api" {
+	metadata {
+		name = "notes-api"
+		namespace = "default"
+		labels = {
+			app = "notes-api"
+		}
+	}
+	
+	spec {
+		replicas = 2
+
+		selector {
+			match_labels = {
+				app = "notes-api"
+			}
+		}
+
+		template {
+			metadata {
+				labels = {
+					app = "notes-api"
+				}
+			}
+
+			spec {
+				container {
+					name = "notes-api"
+					image = "notes-api:v1"
+					
+					image_pull_policy = "Never"
+
+					port {
+						container_port = 3000
+					}
+				}
+			}
+		}
+	}
+}
+
+resource "kubernetes_service" "notes-api" {
+	metadata {
+		name = "notes-api"
+		namespace = "default"
+		labels = {
+			environment = "production"
+		}
+	}
+	spec {
+		selector = {
+			app = "notes-api"
+		}
+	
+	port {
+		port = 3000
+		target_port = 3000
+	}
+
+	type = "NodePort"
+	}
+}
+
